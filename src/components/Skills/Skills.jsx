87e9729f6e5f3a06fe5c738cc4357d5436b349b7
Iryna Vovk <irynavovk.s@gmail.com>
2019-04-19 17:36:40 +0300

    ...

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,26 +1,25 @@
-import React, {Component} from 'react';
-import './Skills.css';
+import React, { Component } from "react";
+import "./Skills.css";
 
 class Skills extends Component {
-   
-    render() { 
-        return ( 
-            <section className="blue-body" id="skills">
-                <header>Technical Skills</header>
-                <blockquote>Good understanding of web-related technologies.
-                </blockquote>
-                <div className="container">
-                    <div className="row">
-                        { this.props.skills.map(li => <figure key={li.id} className="box">
-                        <img src={li.img} className="img-size" alt="skills"/>
-                        <figcaption>{li.title}</figcaption>
-                        </figure>)}
-                    </div>
-                </div>
-            </section>
-          
-         );
-    }
+  render() {
+    return (
+      <section className="blue-body" id="skills">
+        <header>Technical Skills</header>
+        <blockquote>Good understanding of web-related technologies.</blockquote>
+        <div className="container">
+          <div className="row">
+            {this.props.skills.map(li => (
+              <figure key={li.id} className="box">
+                <img src={li.img} className="img-size" alt="skills" />
+                <figcaption>{li.title}</figcaption>
+              </figure>
+            ))}
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
- 
+
 export default Skills;
