dd13bf5802a5d8c8271f3283d40488fb1518d412
Iryna Vovk <irynavovk.s@gmail.com>
2019-05-02 17:02:27 +0300

    add project "Weather"

--- src/db.js
+++ src/db.js
@@ -60,14 +60,14 @@ export const db = {
             "period": "Feb 2018 - Present",
             "title": "freeCodeCamp",
             "listTitle": "As a begginer I started to learn code with FreeCodeCamp. This course helped me to acquaint with: ",
-            "li": ["Responsive Web Design", "Javascript Algorithms And Data Structures", "Front End Libraries"]
+            "li": ["Responsive Web Design", "JavaScript Algorithms And Data Structures", "Front-End Libraries"]
         },
         {
             "id": "2",
             "img": require("./Photos/udemy.png"),
             "period": "January 2018",
             "title": "The web developer bootcamp",
-            "listTitle": "Using materials of this course I've learnt how to use HTML, CSS, JS, jQuery and Bootstrap libraries in real projects. The most usefull for me was creating color-game and to-do-list projects. Also I've learnt more about:",
+            "listTitle": "Using materials of this course I've learnt how to use HTML, CSS, JS, jQuery and Bootstrap libraries in real projects. The most useful for me was creating color-game and to-do-list projects. Also I've learnt more about:",
             "li": ["Flexbox and grid system", "Objects and arrays", "Bootstrap"],
         },
         {
@@ -76,7 +76,7 @@ export const db = {
             "period": "January 2019",
             "title": "Create a Blog with WordPress",
             "listTitle": "I've got this course to understand how Wordpress works and what it is in general. I've learnt about: ",
-            "li": ["Setting up site (writing, reading, comments)", "Creating new blocks with differnt elements and styling it", "Using widgest and plugins"],
+            "li": ["Setting up site (writing, reading, comments)", "Creating new blocks with differnt elements and styling it", "Using widgets and plugins"],
         }
     ],
     "projects": [{
@@ -84,7 +84,7 @@ export const db = {
             "img":  require("./Photos/invitation.png"),
             "title": "Site-invitation",
             "content": ["jQuery", "HTML", "CSS"],
-            "description": "This project was created using HTML, CSS and jQuery library. This is a wedding site-invitation, which contains details about date, marrige couple, bridesmaids and groomsmen, time and location of holding a wedding ceremony.",
+            "description": "This project was created using HTML, CSS and jQuery library. This is a wedding site-invitation, which contains details about date, marriage couple, bridesmaids and groomsmen, time and location of holding a wedding ceremony.",
             "github": "https://github.com/irynahotsiy/site-invitation",
             "demo": "https://irynahotsiy.github.io/site-invitation"
         },
@@ -93,7 +93,7 @@ export const db = {
             "img":  require("./Photos/todo.png"),
             "title": "To-do-list",
             "content": ["React JS", "HTML", "Styled Components"],
-            "description": "Simple Todo list was created using create-react-app. Instead of plain css I used Styled Components.",
+            "description": "Simple Todo-List was created using create-react-app. Instead of plain css I used Styled Components.",
             "github": "https://github.com/irynahotsiy/to-do-list",
             "demo": "https://toddddo-list.netlify.com"
         },
@@ -116,6 +116,15 @@ export const db = {
             "github": "https://github.com/irynahotsiy/color-game-react",
             "demo": "https://color-game-react.netlify.com"
 
+        },
+        {
+            "id": "5",
+            "img": require("./Photos/weather.png"),
+            "title": "Weather-App",
+            "content": ["React JS", "HTML", "Styled Components"],
+            "description": "Simple Weather-App created using create-react-app and Styled Components.",
+            "github": "https://github.com/irynahotsiy/Weather",
+            "demo": "https://weather-coords.netlify.com"
         }
     ],
     "social": [{
